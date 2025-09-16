2e83ee241250361fce9d567b4e9f0944f0b0d195
kingytkryt <kingytkryt@gmail.com>
2025-09-16 10:55:28 +0300

    Initial commit

---
+++ first.py
@@ -0,0 +1,9 @@
+a = int(input("Введіть значення а: "))
+b = int(input("Введіть значення b: "))
+if a>b:
+    x = b*a-1
+elif a==b:
+    x=-10
+else:
+    x=(a-5)/b
+print("X=",x)
---
+++ third.py
@@ -0,0 +1,5 @@
+N = int(input("Введіть 1<=N<=9: "))
+for i in range(N,0,-1):
+    print((str(N)+" ")*i)
+for i in range(2,N+1):
+    print((str(N)+" ")*i)

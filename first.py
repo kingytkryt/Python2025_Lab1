42fbbfbb6e6e8636976556888c8a46493654a2cd
kingytkryt <48378566+kingytkryt@users.noreply.github.com>
2025-09-17 15:53:01 +0300

    first.py

--- first.py
+++ first.py
@@ -1,5 +1,9 @@
-a = int(input("Введіть значення а: "))
-b = int(input("Введіть значення b: "))
+a = int(input ("Введіть а: "))
+while (a<=0):
+    a = int(input ("Введіть ще раз а: "))
+b = int(input ("Введіть b: "))
+while (b<=0):
+    b = int(input ("Введіть ще раз b: "))
 if a>b:
     x = b*a-1
 elif a==b:
